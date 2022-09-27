e2a77270f8702fdf9098ac2add45eeaf32094951
Ed1Abreu <edvandersonabreu161@gmail.com>
2022-09-27 13:25:59 -0300

    Publicação formulário

---
+++ js/botao.js
@@ -0,0 +1,8 @@
+const btn =document.querySelector('button')
+btn.addEventListener('mouseover', (e) =>{
+    const x = e.pageX - btn.offsetLeft;
+    const y = e.pageY - btn.offsetTop;
+
+    btn.style.setProperty('--x', x + 'px');
+    btn.style.setProperty('--y', y + 'px');
+})
